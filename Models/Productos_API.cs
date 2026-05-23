bb8743677d2bb7ebaec9692109dd7d0f58bae544
zsff28 <sofia.f.flores.a@gmail.com>
2026-05-23 11:18:19 -0600

    Cambio a WebAssambley

---
+++ Models/Productos_API.cs
@@ -0,0 +1,16 @@
+﻿namespace Restaurante.Models
+{
+    public class Productos_API
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; } = "";
+        public string Descripcion { get; set; } = "";
+        public decimal Precio { get; set; }
+        public int Calorias { get; set; }
+        public bool Disponible { get; set; }
+        public string Categoria { get; set; } = "";
+        public int CategoriaId { get; set; }
+    }
+
+
+}
--- Program.cs
+++ Program.cs
@@ -13,6 +13,8 @@ builder.Services.AddDbContext<RestauranteDbContext>(options =>
         builder.Configuration
             .GetConnectionString("DefaultConnection")));
 
+builder.Services.AddHttpClient();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
