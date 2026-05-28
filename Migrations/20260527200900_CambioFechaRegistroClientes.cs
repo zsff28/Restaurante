89527e06683c6c8f7893077f912616bc3c9bf58b
zsff28 <sofia.f.flores.a@gmail.com>
2026-05-27 18:52:25 -0600

    Migraciones (cambios de datos en tablas de Azure)

---
+++ Migrations/20260527200900_CambioFechaRegistroClientes.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Restaurante.Migrations
+{
+    /// <inheritdoc />
+    public partial class CambioFechaRegistroClientes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateOnly>(
+                name: "FechaRegistro",
+                table: "Clientes",
+                type: "date",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "FechaRegistro",
+                table: "Clientes",
+                type: "datetime2",
+                nullable: false,
+                oldClrType: typeof(DateOnly),
+                oldType: "date");
+        }
+    }
+}
