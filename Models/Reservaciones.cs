f6c070a172562addbc4d12045ca9daac47b8d24f
zsff28 <sofia.f.flores.a@gmail.com>
2026-05-29 10:46:05 -0600

    Correcciones y se agregan algunos comentarios

--- Models/Reservaciones.cs
+++ Models/Reservaciones.cs
@@ -7,7 +7,7 @@
         // Clave Foránea
         public int ClienteId { get; set; }
 
-        // Propiedad de navegación: Un pedido pertenece a un cliente
+        // Propiedad de navegación. Define la relación con la tabla (un reservación pertenece a un cliente). Permite conocer los valores de clientes.
         public virtual Cliente? Cliente { get; set; }
         public DateTime Fecha { get; set; }
         public decimal? Total { get; set; }
